36b08fca5efb239ea98c4b07a5e8f7d8ef0d431d
allisontruong <allisontruong@mail.weber.edu>
2017-12-14 09:31:47 -0700

    added more stuff

---
+++ check_profanity.py
@@ -0,0 +1,45 @@
+import urllib.request
+import urllib.parse
+
+def check_profanity(text_to_check):
+    """
+    Check profanity words from a text file
+    Connect to:
+    "http://wdylike.appspot.com/?q="
+    to check for profanity
+    :param text_to_check: text to check
+    :return: True or False
+    """
+    url_api = "http://wdylike.appspot.com/?q="
+    conn = urllib.request.urlopen(url_api + urllib.parse.quote(text_to_check))
+
+    out = conn.read()
+    if out.decode('utf-8') == "false":
+        return False
+    else:
+        return True
+
+
+def read_text():
+    with open('movie_quotes.txt', mode='rt', encoding='utf-8') as f:
+        text = []
+        for line in f:
+            text.append(line.strip())
+
+    # Call check profanity
+    if check_profanity(line.strip()):
+        print("profanity alert")
+    else:
+        print("This document has no curse words")
+
+
+def main():
+    """
+    Test Function
+    """
+    read_text()
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
